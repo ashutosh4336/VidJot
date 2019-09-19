3219881ae4d3eea79ddeefa08d3cd08b885abc55
Ashutosh Panda <ashu.juga99@gmail.com>
2019-09-19 10:33:39 +0530

    BootStrap Added

--- app.js
+++ app.js
@@ -3,19 +3,11 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-//           Middleware            //
-// app.use(function(req, res, next) {
-//   //   console.log(Date.now());
-//   req.name = 'Ashutosh Panda';
-//   next();
-// });
-
 //Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  //   console.log(req.name);
   res.render('index');
 });
 
